ef61ae4bcd124c00692b9d1ae295d72e7fedab81
Gabriel Maffei de Almeida <gabriel.maffei.alm@edu.univali.br>
2023-04-24 17:19:47 -0300

    Adicionado novo main.cpp

--- TesteInicialGab/LeituraDeArquivos.cpp
+++ TesteInicialGab/LeituraDeArquivos.cpp
@@ -2,21 +2,37 @@
 #include <fstream>
 #include <vector>
 #include <GL/freeglut.h>
+#include "glm/glm.hpp"
 #include <string>
 using namespace std;
 
 //globals
 
 unsigned int elefante;
-vector<vector<float>> vertices;
-vector<vector<int>> faces;
-float rot_ele;
+struct Face {
+	vector<int> vi;
+	vector<int> ti;
+	vector<int> ni;
+};
 
+Face criar_face(vector<int> vi, vector<int> ti, vector<int> ni) {
+	Face face;
+	face.vi = vi;
+	face.ti = ti;
+	face.ni = ni;
+	return face;
+}
+
+vector<vector<float>> vertices;
+vector<Face> faces;
+vector<vector<float>> normais;
+vector<vector<float>> texturas;
+float rot_ele;
 
 void loadObj(string fname)
 {
     int read;
-    float x, y, z;
+    //float x, y, z;
     ifstream arquivo(fname);
     if (!arquivo.is_open()) {
         cout << "arquivo nao encontrado";
@@ -24,34 +40,101 @@ void loadObj(string fname)
     }
     else {
         string tipo;
-        while (arquivo >> tipo)
-        {
+		while (arquivo >> tipo)
+		{
+			if (tipo == "f")
+			{
+				string a, b, c;
+				arquivo >> a >> b >> c;
 
-            if (tipo == "v")
-            {
-                vector<float> vertice;
-                float x, y, z;
-                arquivo >> x >> y >> z;
-                vertice.push_back(x);
-                vertice.push_back(y);
-                vertice.push_back(z);
-                vertices.push_back(vertice);
-            }
+				int v1, v2, v3;
+				int t1, t2, t3;
+				int n1, n2, n3;
 
-            if (tipo == "f")
-            {
-                vector<int> face;
-                string x, y, z;
-                arquivo >> x >> y >> z;
-                int fp = stoi(x.substr(0, x.find("/"))) - 1;
-                int fs = stoi(y.substr(0, y.find("/"))) - 1;
-                int ft = stoi(z.substr(0, z.find("/"))) - 1;
-                face.push_back(fp);
-                face.push_back(fs);
-                face.push_back(ft);
-                faces.push_back(face);
-            }
-        }
+				v1 = stoi(a.substr(0, a.find("/"))) - 1;
+				a.erase(0, a.find("/") + 1);
+
+				if (a.find("/") != 0) {
+					t1 = stoi(a.substr(0, a.find("/"))) - 1;
+				}
+				else {
+					t1 = -1;
+				}
+
+				a.erase(0, a.find("/") + 1);
+				n1 = stoi(a) - 1;
+				v2 = stoi(b.substr(0, b.find("/"))) - 1;
+				b.erase(0, b.find("/") + 1);
+
+				if (b.find("/") != 0) {
+					t2 = stoi(b.substr(0, b.find("/"))) - 1;
+				}
+				else {
+					t2 = -1;
+				}
+
+				b.erase(0, b.find("/") + 1);
+				n2 = stoi(b) - 1;
+				v3 = stoi(c.substr(0, c.find("/"))) - 1;
+				c.erase(0, c.find("/") + 1);
+
+				if (c.find("/") != 0) {
+					t3 = stoi(c.substr(0, c.find("/"))) - 1;
+				}
+				else {
+					t3 = -1;
+				}
+
+				c.erase(0, c.find("/") + 1);
+				n3 = stoi(c) - 1;
+
+				vector<int> vi;
+				vector<int> ti;
+				vector<int> ni;
+
+				vi.push_back(v1);
+				vi.push_back(v2);
+				vi.push_back(v3);
+
+				ti.push_back(t1);
+				ti.push_back(t2);
+				ti.push_back(t3);
+
+				ni.push_back(n1);
+				ni.push_back(n2);
+				ni.push_back(n3);
+
+				faces.push_back(criar_face(vi, ti, ni));
+			}
+			else if (tipo == "v")
+			{
+				vector<float> vertice;
+				float x, y, z;
+				arquivo >> x >> y >> z;
+				vertice.push_back(x);
+				vertice.push_back(y);
+				vertice.push_back(z);
+				vertices.push_back(vertice);
+			}
+			else if (tipo == "vn")
+			{
+				vector<float> normal;
+				float x, y, z;
+				arquivo >> x >> y >> z;
+				normal.push_back(x);
+				normal.push_back(y);
+				normal.push_back(z);
+				normais.push_back(normal);
+			}
+			else if (tipo == "vt") {
+				vector<float> textura;
+				float x, y;
+				arquivo >> x >> y;
+				textura.push_back(x);
+				textura.push_back(y);
+				texturas.push_back(textura);
+			}
+		}
     }
 
 
@@ -62,7 +145,7 @@ void loadObj(string fname)
     glNewList(elefante, GL_COMPILE);
     {
         glPushMatrix();
-        glBegin(GL_LINES);
+        glBegin(GL_QUADS);
 
         for (int i = 0; i < faces.size(); i++)
         {
---
+++ TesteInicialGab/mainNovo.cpp
@@ -0,0 +1,269 @@
+#include <GL/freeglut.h>
+#include "glm/glm.hpp"
+#include "glm/gtx/component_wise.hpp"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+using namespace glm;
+
+struct Vertex
+{
+    vec3 posicao; //v
+    vec2 textura; //vt
+    vec3 normal; //vn
+};
+
+struct VertRef //f
+{
+    VertRef(int v, int vt, int vn)
+        : v(v), vt(vt), vn(vn)
+    { }
+    int v, vt, vn;
+};
+
+vector< Vertex > LoadOBJ(istream& in)
+{
+    vector< Vertex > verts;
+
+    vector< vec4 > posicoes(1, vec4(0, 0, 0, 0)); //x, y, z, w (posicao normalizada)
+    vector< vec3 > texturas(1, vec3(0, 0, 0));
+    vector< vec3 > normais(1, vec3(0, 0, 0));
+    string lineStr;
+    while (getline(in, lineStr))
+    {
+        istringstream lineSS(lineStr);
+        string lineType;
+        lineSS >> lineType;
+
+        // vertice - posicao
+        if (lineType == "v")
+        {
+            float x = 0, y = 0, z = 0, w = 1;
+            lineSS >> x >> y >> z >> w;
+            posicoes.push_back(vec4(x, y, z, w));
+        }
+
+        // textura - coordenada da textura png
+        if (lineType == "vt")
+        {
+            float u = 0, v = 0, w = 0;
+            lineSS >> u >> v >> w;
+            texturas.push_back(vec3(u, v, w));
+        }
+
+        // normal - mapa de normais
+        if (lineType == "vn")
+        {
+            float i = 0, j = 0, k = 0;
+            lineSS >> i >> j >> k;
+            normais.push_back(normalize(vec3(i, j, k)));
+        }
+
+        // faces - poligonos
+        if (lineType == "f")
+        {
+            vector< VertRef > refs;
+            string refStr;
+            while (lineSS >> refStr)
+            {
+                istringstream ref(refStr);
+                string vStr, vtStr, vnStr;
+                getline(ref, vStr, '/');
+                getline(ref, vtStr, '/');
+                getline(ref, vnStr, '/');
+                int v = atoi(vStr.c_str());
+                int vt = atoi(vtStr.c_str());
+                int vn = atoi(vnStr.c_str());
+                v = (v >= 0 ? v : posicoes.size() + v);
+                vt = (vt >= 0 ? vt : texturas.size() + vt);
+                vn = (vn >= 0 ? vn : normais.size() + vn);
+                refs.push_back(VertRef(v, vt, vn));
+            }
+
+            if (refs.size() < 3)
+            {
+                // ignorar linhas abreviadas
+                continue;
+            }
+
+            // calculo da normal
+            // assume que é convexo e coplanar
+            VertRef* p[3] = { &refs[0], NULL, NULL };
+            for (size_t i = 1; i + 1 < refs.size(); ++i)
+            {
+                p[1] = &refs[i + 0];
+                p[2] = &refs[i + 1];
+
+                // referência http://www.opengl.org/wiki/Calculating_a_Surface_Normal
+                vec3 U(posicoes[p[1]->v] - posicoes[p[0]->v]);
+                vec3 V(posicoes[p[2]->v] - posicoes[p[0]->v]);
+                vec3 faceNormal = normalize(cross(U, V));
+
+                for (size_t j = 0; j < 3; ++j)
+                {
+                    Vertex vert;
+                    vert.posicao = vec3(posicoes[p[j]->v]);
+                    vert.textura = vec2(texturas[p[j]->vt]);
+                    vert.normal = (p[j]->vn != 0 ? normais[p[j]->vn] : faceNormal);
+                    verts.push_back(vert);
+                }
+            }
+        }
+    }
+
+    return verts;
+}
+
+int btn;
+ivec2 startMouse;
+ivec2 startRot, curRot;
+ivec2 startTrans, curTrans;
+void mouse(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        btn = button;
+        startMouse = ivec2(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+        startRot = curRot;
+    }
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+    {
+        btn = button;
+        startMouse = ivec2(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+        startTrans = curTrans;
+    }
+}
+
+void motion(int x, int y)
+{
+    ivec2 curMouse(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
+    if (btn == GLUT_LEFT_BUTTON)
+    {
+        curRot = startRot + (curMouse - startMouse);
+    }
+    else if (btn == GLUT_RIGHT_BUTTON)
+    {
+        curTrans = startTrans + (curMouse - startMouse);
+    }
+    glutPostRedisplay();
+}
+
+vector< Vertex > model;
+void display()
+{
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    double w = glutGet(GLUT_WINDOW_WIDTH);
+    double h = glutGet(GLUT_WINDOW_HEIGHT);
+    double ar = w / h;
+    glTranslatef(curTrans.x / w * 2, curTrans.y / h * 2, 0);
+    gluPerspective(60, ar, 0.1, 100);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0, 0, -10);
+
+    glPushMatrix();
+    {
+        glRotatef(curRot.x % 360, 0, 1, 0);
+        glRotatef(-curRot.y % 360, 1, 0, 0);
+
+        // Modelo - .obj
+        glColor3ub(255, 0, 0);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glEnableClientState(GL_NORMAL_ARRAY);
+        glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &model[0].posicao);
+        glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &model[0].textura);
+        glNormalPointer(GL_FLOAT, sizeof(Vertex), &model[0].normal);
+        glDrawArrays(GL_TRIANGLES, 0, model.size());
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+        glDisableClientState(GL_NORMAL_ARRAY);
+
+        // Iluminação
+        glDisable(GL_LIGHTING);
+        glEnable(GL_LIGHTING);
+    }
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+// return the min/max points of pts
+template< typename Vec >
+pair< Vec, Vec > GetExtents(const Vec* pts, size_t stride, size_t count)
+{
+    unsigned char* base = (unsigned char*)pts;
+    Vec pmin(*(Vec*)base);
+    Vec pmax(*(Vec*)base);
+    for (size_t i = 0; i < count; ++i, base += stride)
+    {
+        const Vec& pt = *(Vec*)base;
+        pmin = glm::min(pmin, pt);
+        pmax = glm::max(pmax, pt);
+    }
+
+    return make_pair(pmin, pmax);
+}
+
+// centers geometry around the origin
+// and scales it to fit in a size^3 box
+template< typename Vec >
+void CenterAndScale(Vec* pts, size_t stride, size_t count, const typename Vec::value_type& size)
+{
+    typedef typename Vec::value_type Scalar;
+
+    // get min/max extents
+    pair< Vec, Vec > exts = GetExtents(pts, stride, count);
+
+    // center and scale 
+    const Vec center = (exts.first * Scalar(0.5)) + (exts.second * Scalar(0.5f));
+
+    const Scalar factor = size / glm::compMax(exts.second - exts.first);
+    unsigned char* base = (unsigned char*)pts;
+    for (size_t i = 0; i < count; ++i, base += stride)
+    {
+        Vec& pt = *(Vec*)base;
+        pt = ((pt - center) * factor);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ifstream ifile("data/radar.obj");
+    model = LoadOBJ(ifile);
+    CenterAndScale(&model[0].posicao, sizeof(Vertex), model.size(), 7);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("OBJ");
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+
+    glEnable(GL_DEPTH_TEST);
+
+    // set up "headlamp"-like light
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    GLfloat posicao[] = { 0, 0, 1, 0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, posicao);
+
+    glPolygonMode(GL_FRONT, GL_FILL);
+    glPolygonMode(GL_BACK, GL_LINE);
+    glutMainLoop();
+    return 0;
+}
